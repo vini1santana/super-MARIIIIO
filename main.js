83720a5973e6da6a7a72db04e2ed537bab1d1e22
vinicius <163008525+vini1santana@users.noreply.github.com>
2024-08-26 10:32:09 -0300

    .

--- main.js
+++ main.js
@@ -1,11 +1,43 @@
 const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
 const jump = () => {
-    mario.classList.add('jump');
+    mario.classList.add("jump");
 }
- document.addEventListener('keydown',jump);
+setTimeout(() => {
+    mario.classList.remove("jump");
+
+} 
+,500);
+}
+
+const loop = setInterval (() => {});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.addEventListener('keydown',jump);
 
  
-const pipe
+
 
 
