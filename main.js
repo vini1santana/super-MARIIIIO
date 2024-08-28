0455ba30d2c1d1be870df03b350e671299be1620
vinicius <163008525+vini1santana@users.noreply.github.com>
2024-08-28 11:29:13 -0300

    .

--- main.js
+++ main.js
@@ -6,34 +6,28 @@ const jump = () => {
 }
 setTimeout(() => {
     mario.classList.remove("jump");
-
 } 
 ,500);
 }
 
-const loop = setInterval (() => {});
+const loop = setInterval (() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
 
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
 
+        mario.style.animation = "none";
+        mario.style.left = `${pipePosition}px`;
+    }
+    mario.src = "./img/game-over.png";
+    mario.style.width = "75px";
+    mario.style.marginLeft = "50px";
 
+    clearInterval(loop);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+},10);
 
 document.addEventListener('keydown',jump);
 
