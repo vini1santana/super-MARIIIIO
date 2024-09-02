d683ba00b439885c6a240f2c0b6fd2417302d169
vinicius <163008525+vini1santana@users.noreply.github.com>
2024-09-02 07:31:00 -0300

    .

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 const mario = document.querySelector(".mario");
-const pipe = document.querySelector(".pipe");
+const pipe = document.querySelector(".pipe")
 
 const jump = () => {
     mario.classList.add("jump");
-}
-setTimeout(() => {
-    mario.classList.remove("jump");
-} 
-,500);
+
+    setTimeout(() => {
+        mario.classList.remove("jump")
+    }
+    ,500);
 }
 
 const loop = setInterval (() => {
-    const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px","");
+const pipePosition = pipe.offsetLeft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
 
-    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
-        pipe.style.animation = "none";
-        pipe.style.left = `${pipePosition}px`;
+if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
 
-        mario.style.animation = "none";
-        mario.style.left = `${pipePosition}px`;
-    }
-    mario.src = "./img/game-over.png";
+    mario.style.animation = "none";
+    mario.style.left = `${marioPosition}px`;
+
+    mario.src = "game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "50px";
 
@@ -29,9 +29,4 @@ const loop = setInterval (() => {
 
 },10);
 
-document.addEventListener('keydown',jump);
-
- 
-
-
-
+document.addEventListener("keydown", jump);
