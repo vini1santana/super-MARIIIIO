7343efc4d1b5897ffe594a144487e97d7d306d11
vinicius <163008525+vini1santana@users.noreply.github.com>
2024-09-23 10:58:53 -0300

    .

--- main.js
+++ main.js
@@ -38,7 +38,11 @@ const jump = () => {
 const updateScore = () => {
   score += 1;
   scoreElement.textContent = score;
+  const animation = 1.5/ (1 + score/ 500);
+  pipe.style.animation = pipe-animation ${animationSpeed}s infinite linear ;
+
 }
+
 const loop = setInterval (() => {
 const pipePosition = pipe.offsetLeft;
 const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
